4edad3dc2664e3a6379d54a10ba4699d74327918
BatoBudaev <Axl206311>
2024-02-28 00:40:59 +0700

    init

---
+++ Two_Sum/Two Sum.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	n := len(nums)
+
+	for i := 0; i < n; i++ {
+		for j := 1; j < n; j++ {
+			if i == j {
+				break
+			}
+
+			if nums[i]+nums[j] == target {
+				output := []int{i, j}
+				return output
+			}
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+
+	output := twoSum(nums, target)
+	fmt.Println(output)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/BatoBudaev/LeetCode
+
+go 1.22rc2
